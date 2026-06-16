dbb75f92eb66f50c97d60ad9951ffe79509ec989
Huzaifa chhipa <huzaifachhipa75@gmail.com>
2026-06-16 10:08:13 +0530

    fix: rename 'COMMING' folder to 'coming' and update imports

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'motion/react';
 import { useNavigate } from 'react-router-dom';
-import C1Img from '../COMMING/C1.jpeg';
+import C1Img from '../coming/C1.jpeg';
 
 const sections = [
   {
